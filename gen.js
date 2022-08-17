3e966cd90303218553fe7449004d2b9aec8a71c9
UWAYONoella <noellawayo2019@gmail.com>
2022-08-17 19:13:50 +0200

    Feat:all

--- gen.js
+++ gen.js
@@ -1,34 +1,42 @@
-const cont=document.getElementsByClassName("container");
+const cont=document.getElementsByClassName("container")[0];
 const n=document.getElementsByClassName("n1");
 const num=Array.from(n);
-const tools=document.getElementsByClassName("tools");
-const font=document.getElementsByClassName("font");
+const tool=document.getElementsByClassName("tools")[0];
+console.log(tool);
+const toolNber=document.getElementsByClassName("toolNber")[0];
 const button=document.getElementById("net");
 const myId=[];
-const toolNumber=myId.length;
-const currenttag=0;
+let toolNumber=myId.length;
+let currenttag=0;
 num[currenttag].classList.add("new");
-console.log(num);
+
 function contain(){
-    tools.addEventListener("click",(e)=>{
+    tool.addEventListener("click",e=>{
         const mage=e.target.getAttribute("src");
-        const id=e.target.getAttribute("id");
+        const ide=e.target.getAttribute("id");
         function Photos(src,id){
-            this.src=src;
+            this.mage=mage;
             this.id=id;
         }
-        const photo=new Photos(src,id);
+        const photo=new Photos(mage,ide);
         myId.push(photo);
-        toolNumber=myId.length;
-        tools.innerText=myId;
+        // toolNumber=myId.length;
+        toolNber.innerText=myId.length;
+        
         
     })
 }
 contain();
+
 button.addEventListener("click",(e)=>{
 currenttag++;
 num[currenttag].classList.add("new");
-tools.innerHTML="";
+// for(let i=0;i<myId.length;i++);{
+//     let nem=myId[i];
+    // tool.innerHTML=`${myId[0].mage}`;
+
+// }
+
 })
 
     
